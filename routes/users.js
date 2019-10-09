1bbc9b265de7c8a5c97e2742272d4e3d06ee2040
jooprima <jooprima@gmail.com>
2019-10-09 19:37:24 +0700

    create view messages

--- routes/users.js
+++ routes/users.js
@@ -20,12 +20,12 @@ router.post("/register", function(req, res) {
   let errors = [];
 
   if (!name || !email || !password || !password2) {
-    errors.push("Silahkan lengkapi data anda !");
+    errors.push({ msg: "Silahkan lengkapi data anda !" });
     console.log("Silahkan lengkapi data anda");
   }
 
   if (password != password2) {
-    errors.push("Password tidak sama");
+    errors.push({ msg: "Password tidak sama" });
     console.log("Password tidak sama");
   }
 
@@ -41,7 +41,7 @@ router.post("/register", function(req, res) {
     User.findOne({ email: email }).then(user => {
       if (user) {
         console.log("Email sudah ada");
-        errors.push("Email Sudah ada");
+        errors.push({ msg: "Email Sudah ada" });
         res.render("register", {
           errors,
           name,
