2a3b1dfc1ab9680f3814730e23b1d6bd94c2ad23
jooprima <jooprima@gmail.com>
2019-10-09 09:29:08 +0700

    setup routes users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Halaman Login
+router.get("/login", function(req, res, next) {
+  res.render("login", { title: "Halaman Login" });
+});
+
+//Halaman Register
+router.get("/register", function(req, res, next) {
+  res.render("register", { title: "Halaman Register" });
 });
 
 module.exports = router;
