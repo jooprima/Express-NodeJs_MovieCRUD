e5aee456d591f0fa022841aa5143ed14a6472cfa
jooprima <jooprima@gmail.com>
2019-10-09 09:19:00 +0700

    setup routes index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//Halaman welcome
+router.get("/", function(req, res, next) {
+  res.render("welcome", { title: "Halaman Welcome" });
+});
+
+//Halaman Dashboard
+router.get("/dashboard", function(req, res, next) {
+  res.render("dashboard", { title: "Halaman Dashboard" });
 });
 
 module.exports = router;
