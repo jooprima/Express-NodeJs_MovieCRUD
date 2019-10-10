703de501795aadf4bccbc829545ab9b40c409f88
jooprima <jooprima@gmail.com>
2019-10-10 10:11:06 +0700

    create view get all movies

--- routes/movies.js
+++ routes/movies.js
@@ -6,7 +6,26 @@ var Movie = require("../models/MovieSchema");
 
 //Get all movies
 router.get("/", function(req, res, next) {
-  res.render("movie/allMovies", { title: "Halaman Get Movies" });
+  let ListMovies = [];
+  Movie.find(function(err, movies) {
+    if (movies) {
+      for (let data of movies) {
+        ListMovies.push({
+          id: data._id,
+          name: data.name,
+          released_on: data.released_on
+        });
+      }
+      res.render("movie/allMovies", { ListMovies });
+    } else {
+      ListMovies.push({
+        id: "",
+        name: "",
+        released_on: ""
+      });
+      res.render("movie/allMovies", { ListMovies });
+    }
+  });
 });
 
 //create Movies
