91eec78147f59836fa3c1ed4e4881176a0a4dc58
jooprima <jooprima@gmail.com>
2019-10-12 21:17:58 +0700

    setup database mongoDB Atlas

--- config/database.js
+++ config/database.js
@@ -1,10 +1,17 @@
 //setup connection to mongodb
 const mongoose = require("mongoose");
-const mongodb = "mongodb://localhost/CRUD_Movie";
+const mongodb =
+  "mongodb://joo:jooprima@crudmovie-shard-00-00-w5jw0.mongodb.net:27017,crudmovie-shard-00-01-w5jw0.mongodb.net:27017,crudmovie-shard-00-02-w5jw0.mongodb.net:27017/crudmovie?ssl=true&replicaSet=crudmovie-shard-0&authSource=admin&retryWrites=true";
+
+// update string url terbaru
+// mongodb://joo:jooprima@crudmovie-shard-00-00-w5jw0.mongodb.net:27017,crudmovie-shard-00-01-w5jw0.mongodb.net:27017,crudmovie-shard-00-02-w5jw0.mongodb.net:27017/crudmovie?ssl=true&replicaSet=crudmovie-shard-0&authSource=admin&retryWrites=true
 
 mongoose
   .connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB Connected"));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// mongoose.connect(...).catch(err => console.log(err))
 
 mongoose.Promise = global.Promise;
 
